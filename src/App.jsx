f0c6a3ce74015b7840aee14887284b9a4cffb651
Eumir Ollarvez <eumirollarvez@gmail.com>
2018-10-04 20:53:27 -0300

    Changing data with the input field

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ export class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			deadLine: 'April 15, 2019'
+			deadLine: 'April 15, 2019',
+			newDeadLine: ''
 		}
 	}
 
 	changeDeadLine() {
 		// Always change the states through the setState method 
-		this.setState({deadLine: 'December 29, 2018'})
+		this.setState({deadLine: this.state.newDeadLine})
 	}
 
 	render() {
-		return (			
+		return (
 			<div className="App">
 				<div className="App-title">Countdown to {this.state.deadLine}</div>
 				<div>
@@ -24,7 +25,10 @@ export class App extends Component {
 					<div className="Clock-minutes">15 minutes</div>
 					<div className="Clock-seconds">30 seconds</div>
 				</div>
-				<input placeholder='new date' />
+				<input
+					placeholder='new date'
+					onChange={event => this.setState({newDeadLine: event.target.value})}
+				/>
 				<button onClick={() => this.changeDeadLine()}>Submit</button>
 			</div>
 			);

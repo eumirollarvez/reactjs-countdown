3a3a00cbef406faf75eb5639623aeb887db7aa8a
Eumir Ollarvez <eumirollarvez@gmail.com>
2018-10-04 20:29:42 -0300

    Structuring the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export class App extends Component {
 	render() {
-		return (
-			<div>Countdown, App Component</div>
-		);
+		return (			
+			<div className="App">
+				<div className="App-title">Countdown to April 15, 2019</div>
+				<div>
+					<div className="Clock-days">15 days</div>
+					<div className="Clock-hours">30 hours</div>
+					<div className="Clock-minutes">15 minutes</div>
+					<div className="Clock-seconds">30 seconds</div>
+				</div>
+				<input placeholder='new date' />
+				<button>Submit</button>
+			</div>
+			);
 	}
 }
 
